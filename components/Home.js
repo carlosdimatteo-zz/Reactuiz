322b15b82945a4f87f780e81873e8a476f04d132
cdimatteo <carlosdima_97@hotmail.com>
2018-11-27 18:28:13 -0400

    fetch questions, started state logic and added navigation

--- components/Home.js
+++ components/Home.js
@@ -3,9 +3,66 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class Home extends React.Component {
 
+
+    constructor(){
+        super()
+        this.state={
+            questions=[],
+            correct=0,
+            currentQuestion:{}
+        }
+    }
+
     static navigationOptions = {
         title: 'Home',
         header: null
       };
 
+      async getData(){
+        await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean')
+        .then(res=>{return res.json()})
+        .then(data=>{
+            console.log('received from API ',data)
+            this.setState({
+                questions:data.results,
+                questionCount:data.results.length(),
+                correct:0,
+                currentQuestion:data.results[0]
+            })
+        })
+    }
+
+    evaluateAnswer(questionIndex,answerString){
+
+        let correctAnswer = this.questions[questionIndex].correct_answer
+        if(correctAnswer==answerString){
+            console.log('answered correctly')
+            this.setState({correct:this.state.correct+1})
+        }else{
+            console.log('answered incorrectly !!')
+        }
+        this.setState({currentQuestion:this.questions[questionIndex+1]})
+    }
+
+    
+        render() {
+
+          return (
+            <View>
+            {this.state.questions.forEach(question,index => {
+                <View>
+                <Text>Question:{question.question} </Text>
+                <Button onPress={()=>this.evaluateAnswer(index,'True')}>
+                    True
+                </Button>
+                <Button onPress={()=>evaluateAnswer(index,'False')}>
+                False
+                </Button>
+                </View>
+            })}
+            </View>
+            
+            
+          )
+        }
   }
