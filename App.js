2f42748ad5f638944c6d535dcf3091ec0d3effa3
cdimatteo <carlosdima_97@hotmail.com>
2018-11-27 18:09:37 -0400

    Initial commit and Home component creation

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Routes/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Routes = createStackNavigator({
+    Home:Home,
+    Edit:NoteEdit,
+    Add:AddNote
+    },{
+      initialRouteName:'Home'
+    });        
